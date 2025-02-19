25223a151c83d3ff49d1851f5ff17a311f366e65
Flacchio25 <flaviano.corsi@gmail.com>
2025-02-19 19:28:28 +0100

    19/02

---
+++ 5. Collections/4-1.py
@@ -0,0 +1,9 @@
+pizze =[ "Pistacchiosa", "Margherita", "Diavola", "Marinara"]
+for pizza in pizze:
+        print(pizza)
+
+for pizza in pizze:
+        print(f"la mia pizza preferita è {pizza}")
+
+print("Mi piacciono i carboidrati \nle proteine \ni grassi no")
+
---
+++ 5. Collections/4-5.py
@@ -0,0 +1,4 @@
+numeri = list(range(1, 1000001))
+print("Min:", min(numeri))
+print("Max:", max(numeri))
+print("Sum:", sum(numeri))
